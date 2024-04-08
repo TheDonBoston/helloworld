f70b4697772eb4f7f352bd1a0eacad3f35a9ba7d
TheDonBoston <159961860+TheDonBoston@users.noreply.github.com>
2024-04-08 13:41:59 -0700

    Add files via upload

---
+++ Brandt_Spenser_expressions.py
@@ -0,0 +1,14 @@
+#CS161 Project 1 -- Math in Python!
+#Spenser Brandt
+#4/7/2024
+#Joseph Jess -- Instructor
+
+#1 Write the following expressions in Python and then find the value of the expression:
+
+# a.)
+x = (2 ** 4 + 7 - 3 * 4) / 5
+print(f"x = {x}")
+
+# b.)
+x = (3 ** 2 + 1) * (16 // 7) / 7
+print(f"x = {x}")
---
+++ Brandt_Spenser_todays_date.py
@@ -0,0 +1,5 @@
+# Import datetime module to capture the date and time
+from datetime import date
+
+today = date.today()
+print("Today's date is: ", today)
